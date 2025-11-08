f6a6ca6426d8f71e6d4e74f7e0da7bc63f9c118d
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2025-11-08 11:49:53 +0530

    moveallzerototheend

--- binarysearch/moveallzerototheend.cpp
+++ binarysearch/moveallzerototheend.cpp
@@ -3,31 +3,31 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"enter the size of array";
-    cin>>n;
-    int arr[n];
-    cout<<"enter the element of array";
-    for(int i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-int index = -1;
-for(int i=0;i<n;i++)
-{
-    if(arr[i]==0 && index == -1)
-    {
-        index = i;
-    }
-    else if(arr[i]!=0 && index !=-1)
-    {
-        swap(arr[i] , arr[index]);
-        index++;
-    }
-}
-for(int i=0;i<n;i++)
-{
-    cout<<arr[i];
-}
-    return 0;
+    cout << "Enter the size of array: ";
+    cin >> n;
 
+    int arr[n];
+    cout << "Enter the elements of array: ";
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int j = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            swap(arr[i], arr[j]);
+            j++;
+        }
+    }
+
+    cout << "Array after moving zeros to the end: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
 }
---
+++ binarysearch/unionoftwosortedarray.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m;
+    cout<<"enter the size of first array";
+    cin>>n;
+    cout<<"enter the size of second array";
+    cin>>m;
+    int arr1[n];
+    cout<<"enter the element of first array";
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr1[i];
+    }
+    int arr2[m];
+    cout<<"enter the element of second array";
+    for(int j=0;j<m;j++)
+    {
+        cin>>arr2[j];
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<arr[j])
+    }
+}
