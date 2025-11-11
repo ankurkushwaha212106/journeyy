5b67f283ed8ed07d8fa4e734763a156efaa256ca
ankurkushwaha212106 <ankurkushwaha212106@gmail.com>
2025-11-11 10:22:19 +0530

    reverse the array

---
+++ binarysearch/reversethearray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    // Reverse using for loop
+    for (int i = 0; i < n / 2; i++)
+    {
+        swap(arr[i], arr[n - i - 1]);
+    }
+
+    cout << "Reversed array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
